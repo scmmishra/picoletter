0ed80f111ccc9805c126e4140a98af9e9ab02f6a
Shivam Mishra <scm.mymail@gmail.com>
2024-03-10 19:57:51 +0530

    feat: enable login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,18 +2,33 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
-      session[:user_id] = user.id
-      redirect_to root_path, notice: 'Logged in successfully.'
+    if user = User.active.authenticate_by(email: params[:email], password: params[:password])
+      start_new_session_for(user)
     else
-      flash.now[:alert] = 'Invalid email or password.'
-      render :new
+      render_rejection :unauthorized
     end
   end
 
   def destroy
     session.delete(:user_id)
-    redirect_to login_path, notice: 'Logged out successfully.'
+    redirect_to login_path, notice: "Logged out successfully."
+  end
+
+  private
+
+  def start_new_session_for(user)
+    user.sessions.create!.tap do |session|
+      authenticated_as session
+    end
+  end
+
+  def render_rejection(status)
+    flash.now[:alert] = "Invalid email or password. Please try again."
+    render :new, status: status
+  end
+
+  def authenticated_as(session)
+    Current.user = session.user
+    cookies.signed.permanent[:session_token] = { value: session.token, httponly: true, same_site: :lax }
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
   has_secure_password :password, validations: true
+
+  has_many :sessions, dependent: :destroy
+
+  scope :active, -> { where(active: true) }
 end
