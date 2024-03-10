b3bf87818909abaf838b33c3f155633c7517eb23
Shivam Mishra <scm.mymail@gmail.com>
2024-03-10 14:38:34 +0530

    feat: add tailwind

--- Gemfile
+++ Gemfile
@@ -65,3 +65,5 @@ group :test do
   gem 'capybara'
   gem 'selenium-webdriver'
 end
+
+gem "tailwindcss-rails", "~> 2.3"
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_05_130848) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_10_090049) do
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "token"
+    t.boolean "active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
