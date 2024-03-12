46e746017e8b3321f03537e9dde4a002658e4968
Shivam Mishra <scm.mymail@gmail.com>
2024-03-12 16:29:43 +0530

    fix: sorting

--- app/controllers/newsletters/posts_controller.rb
+++ app/controllers/newsletters/posts_controller.rb
@@ -3,15 +3,15 @@ class Newsletters::PostsController < ApplicationController
   before_action :set_newsletter
 
   def index
-    @posts = @newsletter.posts.published.order(:published_at)
+    @posts = @newsletter.posts.published.order(published_at: :desc)
   end
 
   def drafts
-    @posts = @newsletter.posts.drafts.order(:updated_at)
+    @posts = @newsletter.posts.drafts.order(updated_at: :desc)
   end
 
   def archive
-    @posts = @newsletter.posts.archived.order(:updated_at)
+    @posts = @newsletter.posts.archived.order(updated_at: :desc)
   end
 
   private
