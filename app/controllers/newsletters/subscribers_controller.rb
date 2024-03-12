f14dfa4ccdf6cc6640663a7782bd63c6eee3e207
Shivam Mishra <scm.mymail@gmail.com>
2024-03-12 18:03:51 +0530

    feat: update table

--- app/controllers/newsletters/subscribers_controller.rb
+++ app/controllers/newsletters/subscribers_controller.rb
@@ -10,7 +10,7 @@ class Newsletters::SubscribersController < ApplicationController
       .order(created_at: :desc)
       .page(params[:page] || 0)
       .where(status: status)
-      .per(50)
+      .per(30)
   end
 
   private
