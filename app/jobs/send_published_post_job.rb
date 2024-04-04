85a7506e9db028117bdda0e5866b53769152d5c8
Shivam Mishra <scm.mymail@gmail.com>
2024-04-04 23:02:40 +0530

    chore: add comments

--- app/jobs/send_published_post_job.rb
+++ app/jobs/send_published_post_job.rb
@@ -1,10 +1,11 @@
+# This job is triggered every 1 minute and it looks ahead 2 minutes to find any posts that are scheduled to be published in the next 2 minutes.
+# This difference in time is to ensure that the job has enough time to process the posts before they are published.
 class SendPublishedPostJob < ApplicationJob
   queue_as :default
 
   def perform
     Rails.logger.info "[SendPublishedPostJob] Sending published post to subscribers"
 
-    # find post which is shceduled in the next 5 minutes
     Post.all.each do |post|
       Rails.logger.info "[SendPublishedPostJob] Sending post #{post.title} to subscribers"
       begin
@@ -20,6 +21,6 @@ class SendPublishedPostJob < ApplicationJob
   end
 
   def posts_to_send
-    Post.drafts.where(scheduled_at: Time.now..(Time.now + 2.minutes))
+    Post.drafts.where(scheduled_at: (Time.now - 2.minutes)..(Time.now + 2.minutes))
   end
 end
