4ffa3bb66289a46a09c99aaec0adde3a69d7249e
Shivam Mishra <scm.mymail@gmail.com>
2024-04-19 18:21:28 +0530

    feat: rename job

--- app/jobs/send_schedule_post_job.rb
+++ app/jobs/send_schedule_post_job.rb
@@ -1,19 +1,19 @@
 # This job is triggered every 1 minute and it looks ahead 2 minutes to find any posts that are scheduled to be published in the next 2 minutes.
 # This difference in time is to ensure that the job has enough time to process the posts before they are published.
-class SendPublishedPostJob < ApplicationJob
+class SendScheduledPost < ApplicationJob
   queue_as :default
 
   def perform
-    Rails.logger.info "[SendPublishedPostJob] Sending published post to subscribers"
+    Rails.logger.info "[SendScheduledPost] Sending published post to subscribers"
 
     posts_to_send.all.each do |post|
-      Rails.logger.info "[SendPublishedPostJob] Sending post #{post.title} to subscribers"
+      Rails.logger.info "[SendScheduledPost] Sending post #{post.title} to subscribers"
       begin
         post.update(status: "processing")
         post.publish_and_send
         post.update(status: "published")
       rescue StandardError => e
-        Rails.logger.error "[SendPublishedPostJob] Error sending post #{post.title}: #{e.message}"
+        Rails.logger.error "[SendScheduledPost] Error sending post #{post.title}: #{e.message}"
         post.update(status: "draft")
         raise e
       end
