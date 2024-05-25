4e3de78c7e15fb0c13437558dffa235f65b96abb
Shivam Mishra <scm.mymail@gmail.com>
2024-05-25 17:19:19 +0530

    fix: payload param

--- spec/jobs/process_resend_webhook_job_spec.rb
+++ spec/jobs/process_resend_webhook_job_spec.rb
@@ -3,24 +3,24 @@ require 'rails_helper'
 RSpec.describe ProcessResendWebhookJob, type: :job do
   let(:email) { create(:email, email_id: "10000", status: :sent) }
   let(:email_id) { email.email_id }
-  let(:product) { email.product }
 
-  describe "#perform" do
-    context "when email is delivered" do
-      let(:payload) do
-        {
-          "type" => "email.delivered",
-          "created_at" => Time.current.iso8601,
-          "data" => {
-            "created_at" => Time.current.iso8601,
-            "email_id" => email_id,
-            "from" => "Acme <onboarding@mail.picoletter.com>",
-            "to" => ["delivered@resend.dev"],
-            "subject" => "Sending this example"
-          }
-        }
-      end
+  let(:email_delivered_payload) do
+    {
+      "type" => "email.delivered",
+      "created_at" => Time.current.iso8601,
+      "data" => {
+        "created_at" => Time.current.iso8601,
+        "email_id" => email_id,
+        "from" => "Acme <onboarding@mail.picoletter.com>",
+        "to" => ["delivered@resend.dev"],
+        "subject" => "Sending this example"
+      }
+    }
+  end
 
+  context "when email is delivered" do
+    it "updates the status" do
+      described_class.perform_now(email_delivered_payload)
       expect(email.reload.status).to eq("delivered")
     end
   end
