08e4ba3ccefdd94d1b3c1203c2f64c93ff66009f
Shivam Mishra <scm.mymail@gmail.com>
2024-07-03 10:54:22 +0530

    fix: add dmarc_records method

--- app/models/concerns/dns_configurable.rb
+++ app/models/concerns/dns_configurable.rb
@@ -1,6 +1,17 @@
 module DNSConfigurable
   include ActiveSupport::Concern
 
+  def dmarc_record
+    {
+      "record" => "DMARC",
+      "name" => "_dmarc",
+      "type" => "TXT",
+      "ttl" => "Auto",
+      "value" => "v=DMARC1; p=none;",
+      "priority" => nil
+    }
+  end
+
   def setup_custom_domain
     return unless use_custom_domain
     return unless saved_change_to_domain?
