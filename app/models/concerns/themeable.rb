a075a91963a85ae60c5a9d71ffbbb96cb410dcfe
Shivam Mishra <scm.mymail@gmail.com>
2024-07-03 10:49:54 +0530

    refactor: split newsletter model concerns

---
+++ app/models/concerns/themeable.rb
@@ -0,0 +1,35 @@
+module Themeable
+  extend ActiveSupport::Concern
+
+  ThemeConfig = Struct.new(:name, :primary, :text_on_primary, :primary_hover, keyword_init: true)
+
+  included do
+    # add a class method to get the theme config
+    def self.theme_config
+      # load colors from conifg/colors.yml
+      data = YAML.load_file(Rails.root.join("config", "colors.yml"))
+      data.map { |item| ThemeConfig.new(item) }
+    end
+  end
+
+  def font_class
+    case font_preference
+    when "serif"
+      "font-serif"
+    when "sans-serif"
+      "font-sans"
+    when "monospace"
+      "font-mono"
+    else
+      "font-serif"
+    end
+  end
+
+  def primary_styles
+    primary = "--pl-primary-color: #{theme.primary};"
+    hover = "--pl-primary-hover-color: #{theme.primary_hover};"
+    text = "--pl-primary-text-color:  #{theme.text_on_primary};"
+
+    "#{primary} #{text} #{hover}"
+  end
+end
--- app/models/newsletter.rb
+++ app/models/newsletter.rb
@@ -37,6 +37,8 @@
 class Newsletter < ApplicationRecord
   include Sluggable
   include Embeddable
+  include Themeable
+  include DNSConfigurable
 
   sluggable_on :title
 
@@ -53,48 +55,12 @@ class Newsletter < ApplicationRecord
 
   attr_accessor :dkim_tokens
 
-  ThemeConfig = Struct.new(:name, :primary, :text_on_primary, :primary_hover, keyword_init: true)
-
   def description_html
     Kramdown::Document.new(description).to_html.html_safe
   end
 
-  def font_class
-    case font_preference
-    when "serif"
-      "font-serif"
-    when "sans-serif"
-      "font-sans"
-    when "monospace"
-      "font-mono"
-    else
-      "font-serif"
-    end
-  end
-
-  def primary_styles
-    primary = "--pl-primary-color: #{theme.primary};"
-    hover = "--pl-primary-hover-color: #{theme.primary_hover};"
-    text = "--pl-primary-text-color:  #{theme.text_on_primary};"
-
-    "#{primary} #{text} #{hover}"
-  end
-
-  def self.theme_config
-    # load colors from conifg/colors.yml
-    data = YAML.load_file(Rails.root.join("config", "colors.yml"))
-    data.map { |item| ThemeConfig.new(item) }
-  end
-
-  def dmarc_record
-    {
-      "record" => "DMARC",
-      "name" => "_dmarc",
-      "type" => "TXT",
-      "ttl" => "Auto",
-      "value" => "v=DMARC1; p=none;",
-      "priority" => nil
-    }
+  def footer_html
+    Kramdown::Document.new(self.email_footer || "").to_html
   end
 
   def sending_from
@@ -107,79 +73,5 @@ class Newsletter < ApplicationRecord
 
   def full_sending_address
     "#{title} <#{sending_from}>"
-  end
-
-  def setup_custom_domain
-    return unless use_custom_domain
-    return unless saved_change_to_domain?
-
-    Rails.logger.info("Setting up custom domain: #{domain}")
-
-    remove_old_domain
-    setup_domain_on_resend
-  end
-
-  def verify_custom_domain
-    return unless use_custom_domain
-
-    Rails.logger.info("Verifying custom domain: #{domain}")
-
-    is_verified_on_dns = verify_dns_records
-    Rails.logger.info("Domain verification on DNS completed. Verified: #{is_verified_on_dns}")
-    return unless is_verified_on_dns
-
-    is_verified, dns_records = verify_domain_on_resend
-    Rails.logger.info("Domain verification on Resend completed. Verified: #{is_verified}")
-    update_columns(domain_verified: is_verified, dns_records: dns_records)
-
-    is_verified
-  end
-
-  def footer_html
-    Kramdown::Document.new(self.email_footer || "").to_html
-  end
-
-  private
-
-  def remove_old_domain
-    return unless domain_id
-
-    Rails.logger.info("Removing old domain: #{domain_id}")
-    resend_service.delete_domain(domain_id) if domain_verified
-    update_columns(domain_id: nil, domain_verified: false)
-    Rails.logger.info("Old domain removed: #{domain_id}")
-  end
-
-  def setup_domain_on_resend
-    Rails.logger.info("Setting up domain on Resend: #{domain}")
-    response = resend_service.create_or_fetch_domain(self.domain, self.domain_id)
-    return unless response
-
-    is_verified = response[:status] == "verified"
-    update_columns(domain_id: response[:id], dns_records: response[:records], domain_verified: is_verified)
-
-    Rails.logger.info("Domain setup completed. Domain ID: #{response[:id]}, Verified: #{is_verified}")
-    response
-  end
-
-  def verify_domain_on_resend
-    return unless domain_id
-    response = resend_service.verify_domain(domain_id)
-    is_verified = response[:status] == "verified"
-
-    [ is_verified, response[:records] ]
-  end
-
-  def verify_dns_records
-    verified = self.dns_records.map do |record|
-      name = "#{record["name"]}.#{domain}"
-      DNSService.verify_record(name, record["value"], record["type"])
-    end
-
-    verified.all?
-  end
-
-  def resend_service
-    ResendDomainService.new
   end
 end
