46e1fe3db94ba816cc43857df73fb921a6154735
Shivam Mishra <scm.mymail@gmail.com>
2024-08-04 13:12:03 +0530

    fix: comment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   config.force_ssl = ActiveModel::Type::Boolean.new.cast(ENV.fetch("FORCE_SSL", false))
   # Skip http-to-https redirect for the default health check endpoint.
-  +   # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
+  # config.ssl_options = { redirect: { exclude: ->(request) { request.path == "/up" } } }
 
   # Log to STDOUT by default
   if ENV.fetch("BETTERSTACK__LOGS_TOKEN", nil)
