4df5c09168f6801fa1da65fca2c4142c348856b8
Shivam Mishra <scm.mymail@gmail.com>
2024-08-04 22:59:18 +0530

    chore: add comments

--- app/controllers/concerns/rate_limiter.rb
+++ app/controllers/concerns/rate_limiter.rb
@@ -4,6 +4,7 @@ module RateLimiter
   class_methods do
     def throttle(to:, within:, only: nil)
       before_action(only: only) do
+        # use remote_ip to rate limit by IP address
         key = "rate_limit:#{request.remote_ip}:#{controller_name}:#{action_name}"
         count = Rails.cache.increment(key, 1, expires_in: within)
 
