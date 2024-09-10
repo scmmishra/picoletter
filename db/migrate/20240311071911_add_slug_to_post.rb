65cf8213161d00b38d7caad613a8e906bc3d47c8
Shivam Mishra <scm.mymail@gmail.com>
2024-09-10 15:26:06 +0530

    fix: migrations

--- db/migrate/20240311071911_add_slug_to_post.rb
+++ db/migrate/20240311071911_add_slug_to_post.rb
@@ -1,6 +1,7 @@
 class AddSlugToPost < ActiveRecord::Migration[7.1]
   def change
-    add_column :posts, :slug, :string, null: false, index: true
-    add_index :posts, %i[newsletter_id slug], unique: true
+    add_column :posts, :slug, :string, null: false
+    add_index :posts, :slug
+    add_index :posts, [ :newsletter_id, :slug ], unique: true
   end
 end
