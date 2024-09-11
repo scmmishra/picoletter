5ed5a0bc8f38f74e0a4ffb4bc64bc574f17e6dea
Shivam Mishra <scm.mymail@gmail.com>
2024-09-11 12:08:23 +0530

    feat: remove unused

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -14,17 +14,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_07_145305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "solid_cache_entries", force: :cascade do |t|
-    t.binary "key", null: false
-    t.binary "value", null: false
-    t.datetime "created_at", null: false
-    t.bigint "key_hash", null: false
-    t.integer "byte_size", null: false
-    t.index ["byte_size"], name: "index_solid_cache_entries_on_byte_size"
-    t.index ["key_hash", "byte_size"], name: "index_solid_cache_entries_on_key_hash_and_byte_size"
-    t.index ["key_hash"], name: "index_solid_cache_entries_on_key_hash", unique: true
-  end
-
   create_table "solid_queue_blocked_executions", force: :cascade do |t|
     t.bigint "job_id", null: false
     t.string "queue_name", null: false
