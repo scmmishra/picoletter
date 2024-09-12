1829ef9d2b0137ff849cb49bed3a927c27dd1323
Shivam Mishra <scm.mymail@gmail.com>
2024-09-12 19:19:25 +0530

    refactor: class default limits

--- app/models/user.rb
+++ app/models/user.rb
@@ -31,16 +31,20 @@ class User < ApplicationRecord
   before_create :activate_user
   before_create :set_basic_limits
 
+  def self.default_limits
+    {
+      subscribers: 1000,
+      emails: 10000
+    }
+  end
+
   def super?
     self.is_superadmin
   end
 
   def limits
     if AppConfig.get("ENABLE_BILLING", false)
-      self[:limits] || {
-        subscribers: 1000,
-        emails: 10000
-      }
+      self[:limits] || self.class.default_limits
     else
       {
         subscribers: Float::INFINITY,
@@ -57,9 +61,6 @@ class User < ApplicationRecord
 
   def set_basic_limits
     return unless AppConfig.get("ENABLE_BILLING", false)
-    self.limits = {
-      subscribers: 1000,
-      emails: 10000
-    }
+    self.limits = self.class.default_limits
   end
 end
