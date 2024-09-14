3386b062f139d3db4f11156b91f428fe598ce63d
Shivam Mishra <scm.mymail@gmail.com>
2024-09-14 12:18:34 +0530

    feat: add more config

--- config/initializers/billing.rb
+++ config/initializers/billing.rb
@@ -1,6 +1,10 @@
 Pico::Billing.setup do |config|
   config.user_class_name = "User"
-  config.lemon_squeezy_api_key = AppConfig.get("LEMON_SQUEEZY_API_KEY", nil)
+  config.lemon_squeezy_api_key = AppConfig.get("LEMON_SQUEEZY__API_KEY", nil)
+  config.store_id = AppConfig.get("LEMON_SQUEEZY__STORE_ID", nil)
+  config.product_id = AppConfig.get("LEMON_SQUEEZY__PRODUCT_ID", nil)
+  config.product_name = AppConfig.get("LEMON_SQUEEZY__PRODUCT_NAME", "Picoletter")
+  config.product_variant_id = AppConfig.get("LEMON_SQUEEZY__PRODUCT_VARIANT_ID", nil)
 end
 
 # Pico::Billing.initialize_lemon_squeezy
