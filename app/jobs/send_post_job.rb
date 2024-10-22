c2a5dd8b8bdc75c089aec27d7a4123df519e3bb7
Shivam Mishra <scm.mymail@gmail.com>
2024-10-22 16:28:51 +0530

    fix: publish

--- app/jobs/send_post_job.rb
+++ app/jobs/send_post_job.rb
@@ -6,9 +6,11 @@ class SendPostJob < ApplicationJob
   def perform(post_id)
     @post = Post.find(post_id)
     @newsletter = @post.newsletter
+    @post.update(status: :processing)
 
     @newsletter.subscribers.verified.find_in_batches(batch_size: BATCH_SIZE) do |batch_subscribers|
       SendPostBatchJob.perform_later(@post.id, batch_subscribers)
     end
+    @post.publish
   end
 end
