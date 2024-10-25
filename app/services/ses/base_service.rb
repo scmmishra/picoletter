b53a0f05d0a6341c8cccd9d419df396bee01f835
Shivam Mishra <scm.mymail@gmail.com>
2024-10-25 16:35:29 +0530

    feat: setup aws creds

--- app/services/ses/base_service.rb
+++ app/services/ses/base_service.rb
@@ -2,6 +2,7 @@ class SES::BaseService
   REGION = "us-east-1".freeze
 
   def initialize
-    @client = Aws::SESV2::Client.new(region: REGION)
+    credentials = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
+    @client = Aws::SESV2::Client.new(region: REGION, credentials: credentials)
   end
 end
