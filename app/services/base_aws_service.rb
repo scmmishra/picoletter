b0f6fb46c4e77f4b6d7938b0af6e58c6194db094
Shivam Mishra <scm.mymail@gmail.com>
2024-10-25 16:47:14 +0530

    feat: use AppConfig

--- app/services/base_aws_service.rb
+++ app/services/base_aws_service.rb
@@ -2,8 +2,13 @@ class BaseAwsService
   REGION = "us-east-1".freeze
 
   def initialize
-    region = ENV["AWS_REGION"] || REGION
-    credentials = Aws::Credentials.new(ENV["AWS_ACCESS_KEY_ID"], ENV["AWS_SECRET_ACCESS_KEY"])
+    region = AppConfig.get("AWS_REGION", REGION)
+    key = AppConfig.get!("AWS_ACCESS_KEY_ID")
+    secret = AppConfig.get!("AWS_SECRET_ACCESS_KEY")
+
+    credentials = Aws::Credentials.new(key, secret)
     @ses_client = Aws::SESV2::Client.new(region:, credentials:)
   end
+
+  private
 end
