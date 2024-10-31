252e2aaa1c2636598a5530d5f4a06d3d1d3e5c28
Shivam Mishra <scm.mymail@gmail.com>
2024-10-31 21:57:16 +0530

    refactor: better docs and reorg

--- app/services/ses/email_service.rb
+++ app/services/ses/email_service.rb
@@ -1,42 +1,47 @@
 class SES::EmailService < BaseAwsService
-  # {
-  #   to: [emails],
-  #   from: someemail@example.com,
-  #   reply_to: reply@example.com,
-  #   subject: "Some subject",
-  #   html: "HTML content",
-  #   text: "Text content",
-  #   headers: {
-  #     'List-Unsubscribe': "<url>",
-  #   }
-  # }
+  # Send an email using Amazon SES
+  #
+  # @param [Hash] params Email parameters
+  # @option params [Array<String>] :to Recipient email addresses
+  # @option params [String] :from Sender email address
+  # @option params [String] :reply_to Reply-to email address
+  # @option params [String] :subject Email subject line
+  # @option params [String] :html HTML email content
+  # @option params [String] :text Plain text email content
+  # @option params [Hash<String, String>] :headers Additional email headers
+  #
+  # @return [AWS::SES::Types::SendEmailResponse] Response from SES API
+  #
+  # @example Basic usage
+  #   email_service.send(
+  #     to: ['user@example.com'],
+  #     from: 'sender@example.com',
+  #     reply_to: 'reply@example.com',
+  #     subject: 'Hello',
+  #     html: '<p>HTML content</p>',
+  #     text: 'Text content',
+  #     headers: {'List-Unsubscribe' => '<url>'}
+  #   )
   def send(params)
-    parsed_headers = params[:headers].map do |key, value|
-      { name: key, value: value }
-    end
+    # Convert header hash to SES format
+    parsed_headers = params.fetch(:headers, {}).map { |key, value| { name: key, value: value } }
 
-    @ses_client.send_email(
+    email_payload = {
       from_email_address: params[:from],
-      destination: {
-        to_addresses: params[:to]
-      },
+      destination: { to_addresses: params[:to] },
       reply_to_addresses: params[:reply_to],
       content: {
         simple: {
-          subject: {
-            data: params[:subject]
-          },
+          subject: { data: params[:subject] },
           body: {
-            text: {
-              data: params[:text]
-            },
-            html: {
-              data: params[:html]
-            }
+            text: { data: params[:text] },
+            html: { data: params[:html] }
           },
           headers: parsed_headers
         }
       }
-    )
+    }
+
+    @ses_client.send_email(email_payload)
   end
 end
