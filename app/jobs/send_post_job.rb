bebc9a8c2f69c52bdc99141ba62cae3fb00a77fe
Shivam Mishra <scm.mymail@gmail.com>
2024-11-01 19:09:54 +0530

    feat: track publish count (#35)

--- app/jobs/send_post_job.rb
+++ app/jobs/send_post_job.rb
@@ -8,9 +8,11 @@ class SendPostJob < ApplicationJob
     @newsletter = @post.newsletter
     @post.update(status: :processing)
 
+    total_batches = (@newsletter.subscribers.verified.count.to_f / BATCH_SIZE).ceil
+    Rails.cache.write("post_#{post_id}_batches_remaining", total_batches)
+
     @newsletter.subscribers.verified.find_in_batches(batch_size: BATCH_SIZE) do |batch_subscribers|
       SendPostBatchJob.perform_later(@post.id, batch_subscribers)
     end
-    @post.publish
   end
 end
