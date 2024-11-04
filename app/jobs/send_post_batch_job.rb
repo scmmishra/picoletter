7deea1dc98e2a1784fc0ddb8bb9f5ec40daf36e3
Shivam Mishra <scm.mymail@gmail.com>
2024-11-04 13:57:32 +0530

    feat: use pre-rendered email

--- app/jobs/send_post_batch_job.rb
+++ app/jobs/send_post_batch_job.rb
@@ -7,7 +7,8 @@ class SendPostBatchJob < ApplicationJob
   def perform(post_id, batch_subscribers)
     @post = Post.find(post_id)
     @newsletter = @post.newsletter
-    @html_content = render_html_content
+    @html_content = get_html_content
+    @text_content = get_text_content
     @from_email = @newsletter.full_sending_address
 
     send_batch(batch_subscribers)
@@ -24,46 +25,65 @@ class SendPostBatchJob < ApplicationJob
 
   def send_batch(batch_subscribers)
     Rails.logger.info "[PostMailer] Sending #{@post.title} to #{batch_subscribers.count} subscribers"
-    batch_params = batch_subscribers.map do |subscriber|
-      prepare_email_payload(subscriber)
-    end
+    sends = batch_subscribers.map do |subscriber|
+      token = subscriber.generate_unsubscribe_token
+      unsub_url = unsubscribe_url(token, @newsletter.slug)
+      html = @html_content.gsub(UNSUBSCRIBE_PLACEHOLDER, unsubscribe_link(unsub_url))
 
-    response = Resend::Batch.send(batch_params)
-    sends = response[:data].map do |payload|
+      response = SES::EmailService.send(
+          to: [ subscriber.email ],
+          from: @from_email,
+          reply_to: @newsletter.reply_to || @newsletter.user.email,
+          subject: @post.title,
+          html: @html_content,
+          text: @text_content,
+          headers: {
+            'List-Unsubscribe': "<#{unsub_url}>",
+            'List-Unsubscribe-Post': "List-Unsubscribe=One-Click",
+            'X-Newsletter-id': "picoletter-#{@newsletter.id}-#{@post.id}-#{subscriber.id}"
+          }
+        )
+
       {
-        email_id: payload["id"],
         post_id: @post.id,
-        status: :sent
+        subscriber_id: subscriber.id,
+        email_id: response.message_id
       }
     end
 
     Email.insert_all(sends)
   end
 
-  def prepare_email_payload(subscriber)
-    token = subscriber.generate_unsubscribe_token
-    unsub_url = unsubscribe_url(token, @newsletter.slug)
-    html = @html_content.gsub(UNSUBSCRIBE_PLACEHOLDER, unsubscribe_link(unsub_url))
-
-    {
-      to: [ subscriber.email ],
-      from: @from_email,
-      reply_to: @newsletter.reply_to || @newsletter.user.email,
-      subject: @post.title,
-      html: html,
-      headers: {
-        'List-Unsubscribe': "<#{unsub_url}>",
-        'List-Unsubscribe-Post': "List-Unsubscribe=One-Click",
-        'X-Newsletter-id': "picoletter-#{@newsletter.id}-#{@post.id}-#{subscriber.id}"
-      }
-    }
+  def get_html_content
+    Rails.cache.fetch(cache_key("html_content")) do
+      rendered_html_content
+    end
   end
 
-  def render_html_content
+  def get_text_content
+    Rails.cache.fetch(cache_key("text_content")) do
+      rendered_text_content
+    end
+  end
+
+  def cache_key(suffix)
+    "post_#{@post.id}_#{suffix}"
+  end
+
+  def rendered_html_content
     ApplicationController.render(
       template: "publish",
-      assigns: { post: @post, newsletter: @newsletter },
-      layout: false
+      assigns: { post: post, newsletter: newsletter },
+      layout: false,
+    )
+  end
+
+  def rendered_text_content
+    ApplicationController.render(
+      template: "publish",
+      assigns: { post: post, newsletter: newsletter },
+      layout: false,
+      formats: [ :text ]
     )
   end
 
