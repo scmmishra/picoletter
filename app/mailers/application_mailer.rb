e448aa2771cc0ef628b190c7046106894e2e3590
Shivam Mishra <scm.mymail@gmail.com>
2024-11-04 13:11:35 +0530

    fix: mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,26 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "from@example.com"
   layout "mailer"
+
+  private
+
+  def notify_address
+    "Picoletter Notifications <notify@#{sending_domain}>"
+  end
+
+  def accounts_address
+    "Picoletter Accounts <accounts@#{sending_domain}>"
+  end
+
+  def support_address
+    "Picoletter Support <accounts@#{sending_domain}>"
+  end
+
+  def no_reply_address
+    "Picoletter <accounts@#{sending_domain}>"
+  end
+
+  def sending_domain
+    AppConfig.get("APP_SENDING_DOMAIN", "picoletter.com")
+  end
 end
