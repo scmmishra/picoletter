7789fdb88a71513cc08adb4054ff3da169058e51
Shivam Mishra <scm.mymail@gmail.com>
2024-11-04 14:35:02 +0530

    feat: use attr reader

--- app/jobs/send_post_batch_job.rb
+++ app/jobs/send_post_batch_job.rb
@@ -1,16 +1,16 @@
 class SendPostBatchJob < BaseSendJob
   queue_as :default
   after_perform :update_batch_count
+  attr_reader :post, :newsletter, :html_content, :text_content, :from_email
 
   UNSUBSCRIBE_PLACEHOLDER = "{{unsubscribe_link}}"
 
   def perform(post_id, batch_subscribers)
     @post = Post.find(post_id)
     @newsletter = @post.newsletter
-    @html_content = rendered_html_content(@post, @newsletter)
-    @text_content = rendered_text_content(@post, @newsletter)
-    @from_email = @newsletter.full_sending_address
-    @ses_service = SES::EmailService.new
+    @html_content = rendered_html_content(post, newsletter)
+    @text_content = rendered_text_content(post, newsletter)
+    @from_email = newsletter.full_sending_address
 
     send_batch(batch_subscribers)
   end
@@ -18,39 +18,43 @@ class SendPostBatchJob < BaseSendJob
   private
 
   def update_batch_count
-    remaining_batches = Rails.cache.decrement(cache_key(@post.id, "batches_remaining"))
-    @post.publish if remaining_batches <= 0
+    remaining_batches = Rails.cache.decrement(cache_key(post.id, "batches_remaining"))
+    post.publish if remaining_batches <= 0
   end
 
   def send_batch(batch_subscribers)
     sends = batch_subscribers.map do |subscriber|
       token = subscriber.generate_unsubscribe_token
-      unsub_url = unsubscribe_url(token, @newsletter.slug)
-      html = @html_content.gsub(UNSUBSCRIBE_PLACEHOLDER, unsubscribe_link(unsub_url))
-      text = @text_content.gsub(UNSUBSCRIBE_PLACEHOLDER, unsub_url)
+      unsub_url = unsubscribe_url(token, newsletter.slug)
+      html = html_content.gsub(UNSUBSCRIBE_PLACEHOLDER, unsubscribe_link(unsub_url))
+      text = text_content.gsub(UNSUBSCRIBE_PLACEHOLDER, unsub_url)
 
-      response = @ses_service.send(
+      response = ses_service.send(
         to: [ subscriber.email ],
-        from: @from_email,
-        reply_to: @newsletter.reply_to.presence || @newsletter.user.email,
-        subject: @post.title,
+        from: from_email,
+        reply_to: newsletter.reply_to.presence || newsletter.user.email,
+        subject: post.title,
         html: html,
         text: text,
         headers: {
           "List-Unsubscribe" => "<#{unsub_url}>",
           "List-Unsubscribe-Post" => "List-Unsubscribe=One-Click",
-          "X-Newsletter-id" => "picoletter-#{@newsletter.id}-#{@post.id}-#{subscriber.id}"
+          "X-Newsletter-id" => "picoletter-#{newsletter.id}-#{post.id}-#{subscriber.id}"
         }
       )
 
       {
-        post_id: @post.id,
+        post_id: post.id,
         subscriber_id: subscriber.id,
         email_id: response.message_id
       }
     end
 
     Email.insert_all(sends)
+  end
+
+  def ses_service
+    @ses_service ||= SES::EmailService.new
   end
 
   def unsubscribe_link(url)
