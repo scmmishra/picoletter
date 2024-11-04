f9a230fe3608cde5316d9db53c9298db7d12b094
Shivam Mishra <scm.mymail@gmail.com>
2024-11-04 13:48:38 +0530

    refactor: reorgainze

--- app/jobs/send_post_job.rb
+++ app/jobs/send_post_job.rb
@@ -1,48 +1,65 @@
 class SendPostJob < ApplicationJob
   queue_as :default
+  attr_reader :post, :newsletter
 
   BATCH_SIZE = 100
 
   def perform(post_id)
-    @post = Post.find(post_id)
-    @newsletter = @post.newsletter
-
-    set_rendered_content_in_cache
-    set_processing
-    send_batches
+    setup_post(post_id)
+    prepare_post_for_sending
+    dispatch_to_subscribers
   end
 
-  def send_batches
-    @newsletter.subscribers.verified.find_in_batches(batch_size: BATCH_SIZE) do |batch_subscribers|
-      SendPostBatchJob.perform_later(@post.id, batch_subscribers)
+  private
+
+  def setup_post(post_id)
+    @post = Post.find(post_id)
+    @newsletter = @post.newsletter
+  end
+
+  def prepare_post_for_sending
+    cache_rendered_content
+    mark_as_processing
+  end
+
+  def dispatch_to_subscribers
+    newsletter.subscribers.verified.find_in_batches(batch_size: BATCH_SIZE) do |batch|
+      SendPostBatchJob.perform_later(post.id, batch)
     end
   end
 
-  def set_rendered_content_in_cache
-    Rails.cache.write("post_#{@post.id}_html_content", render_html_content)
-    Rails.cache.write("post_#{@post.id}_text_content", render_text_content)
+  def cache_rendered_content
+    Rails.cache.write(cache_key("html_content"), rendered_html_content)
+    Rails.cache.write(cache_key("text_content"), rendered_text_content)
   end
 
-  def set_processing
-    @post.update(status: :processing)
-    total_batches = (@newsletter.subscribers.verified.count.to_f / BATCH_SIZE).ceil
-    Rails.cache.write("post_#{@post.id}_batches_remaining", total_batches)
+  def mark_as_processing
+    post.update(status: :processing)
+    Rails.cache.write(cache_key("batches_remaining"), total_batches)
   end
 
-  def render_html_content
+  def total_batches
+    (newsletter.subscribers.verified.count.to_f / BATCH_SIZE).ceil
+  end
+
+  def rendered_html_content
     ApplicationController.render(
       template: "publish",
-      assigns: { post: @post, newsletter: @newsletter },
-      layout: false
+      assigns: { post: post, newsletter: newsletter },
+      layout: false,
     )
   end
 
-  def render_text_content
+  def rendered_text_content
     ApplicationController.render(
       template: "publish",
-      assigns: { post: @post, newsletter: @newsletter },
+      assigns: { post: post, newsletter: newsletter },
       layout: false,
       formats: [ :text ]
     )
+  end
+
+  def cache_key(suffix)
+    "post_#{post.id}_#{suffix}"
   end
 end
