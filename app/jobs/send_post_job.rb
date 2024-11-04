e31be512bb87b1237be69007f4c92100f996b520
Shivam Mishra <scm.mymail@gmail.com>
2024-11-04 13:46:05 +0530

    feat: refactor newsletter

--- app/jobs/send_post_job.rb
+++ app/jobs/send_post_job.rb
@@ -6,13 +6,43 @@ class SendPostJob < ApplicationJob
   def perform(post_id)
     @post = Post.find(post_id)
     @newsletter = @post.newsletter
-    @post.update(status: :processing)
 
-    total_batches = (@newsletter.subscribers.verified.count.to_f / BATCH_SIZE).ceil
-    Rails.cache.write("post_#{post_id}_batches_remaining", total_batches)
+    set_rendered_content_in_cache
+    set_processing
+    send_batches
+  end
 
+  def send_batches
     @newsletter.subscribers.verified.find_in_batches(batch_size: BATCH_SIZE) do |batch_subscribers|
       SendPostBatchJob.perform_later(@post.id, batch_subscribers)
     end
+  end
+
+  def set_rendered_content_in_cache
+    Rails.cache.write("post_#{@post.id}_html_content", render_html_content)
+    Rails.cache.write("post_#{@post.id}_text_content", render_text_content)
+  end
+
+  def set_processing
+    @post.update(status: :processing)
+    total_batches = (@newsletter.subscribers.verified.count.to_f / BATCH_SIZE).ceil
+    Rails.cache.write("post_#{@post.id}_batches_remaining", total_batches)
+  end
+
+  def render_html_content
+    ApplicationController.render(
+      template: "publish",
+      assigns: { post: @post, newsletter: @newsletter },
+      layout: false
+    )
+  end
+
+  def render_text_content
+    ApplicationController.render(
+      template: "publish",
+      assigns: { post: @post, newsletter: @newsletter },
+      layout: false,
+      formats: [ :text ]
+    )
   end
 end
