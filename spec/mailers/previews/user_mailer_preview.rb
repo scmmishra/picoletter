0fa074235e78928c6c99cfa5a2774e6ad243da9f
Shivam Mishra <scm.mymail@gmail.com>
2025-01-24 14:49:39 +0530

    fix: mailer preview

--- spec/mailers/previews/user_mailer_preview.rb
+++ spec/mailers/previews/user_mailer_preview.rb
@@ -1,3 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/user_mailer_mailer
 class UserMailerPreview < ActionMailer::Preview
+  def verify
+    UserMailer.verify(User.first)
+  end
 end
