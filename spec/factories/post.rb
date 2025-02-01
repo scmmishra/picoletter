f884dac3610dde2e854697d522547363d11c6366
Shivam Mishra <scm.mymail@gmail.com>
2025-02-01 14:39:23 +0530

    test: sns webhook job (#49)

--- spec/factories/post.rb
+++ spec/factories/post.rb
@@ -1,12 +1,11 @@
 FactoryBot.define do
   factory(:post) do
+    sequence(:title) { |n| "Title for the post #{n}" }
+    sequence(:slug) { |n| "title-for-the-post-#{n}" }
     content { nil }
     published_at { nil }
     scheduled_at { nil }
-    slug { "title-for-the-post" }
     status { "draft" }
-    title { "Title for the post" }
-
     newsletter
   end
 end
