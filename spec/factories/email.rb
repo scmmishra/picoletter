e33eb0d7989c36e80d34cec847748f6a59ea0033
Shivam Mishra <scm.mymail@gmail.com>
2025-02-01 14:35:37 +0530

    feat: add specs for sns webhook job

--- spec/factories/email.rb
+++ spec/factories/email.rb
@@ -2,9 +2,51 @@ FactoryBot.define do
   factory :subscriber do
     newsletter
     email { Faker::Internet.email }
+    status { :verified }
+    unsubscribed_at { nil }
+    unsubscribe_reason { nil }
+
+    trait :unsubscribed do
+      status { :unsubscribed }
+      unsubscribed_at { Time.current }
+    end
+
+    trait :bounced do
+      status { :unsubscribed }
+      unsubscribed_at { Time.current }
+      unsubscribe_reason { :bounced }
+    end
+
+    trait :complained do
+      status { :unsubscribed }
+      unsubscribed_at { Time.current }
+      unsubscribe_reason { :complained }
+    end
   end
 
   factory :email do
     post
+    subscriber
+    sequence(:id) { |n| "message-#{n}" }  # AWS SES message ID format
+    status { :sent }
+    delivered_at { nil }
+    opened_at { nil }
+    bounced_at { nil }
+    complained_at { nil }
+
+    trait :delivered do
+      status { :delivered }
+      delivered_at { Time.current }
+    end
+
+    trait :bounced do
+      status { :bounced }
+      bounced_at { Time.current }
+    end
+
+    trait :complained do
+      status { :complained }
+      complained_at { Time.current }
+    end
   end
 end
