cbdf21db766c96139972599389237bb154d3e9a7
Shivam Mishra <scm.mymail@gmail.com>
2025-03-09 13:15:15 +0530

    feat: allow creating labels

--- app/controllers/newsletters/labels_controller.rb
+++ app/controllers/newsletters/labels_controller.rb
@@ -8,7 +8,20 @@ class Newsletters::LabelsController < ApplicationController
     @labels = @newsletter.labels
   end
 
+  def create
+    @label = @newsletter.labels.new(label_params)
+    if @label.save
+      redirect_to labels_path(slug: @newsletter.slug), notice: "Label created successfully."
+    else
+      render :new
+    end
+  end
+
   private
+
+  def label_params
+    params.require(:label).permit(:name, :color, :description)
+  end
 
   def set_newsletter
     @newsletter = Newsletter.find_by(slug: params[:slug])
