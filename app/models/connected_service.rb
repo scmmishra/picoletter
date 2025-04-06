1de6d368e4cec77b8bca46f8af8db73bf224e5be
Shivam Mishra <scm.mymail@gmail.com>
2025-04-06 12:47:29 +0530

    feat: allow managin connected services

--- app/models/connected_service.rb
+++ app/models/connected_service.rb
@@ -20,35 +20,38 @@
 #
 class ConnectedService < ApplicationRecord
   belongs_to :user
-  
+
   validates :provider, presence: true
   validates :uid, presence: true
   validates :provider, uniqueness: { scope: :uid }
-  
+
+  scope :google, -> { where(provider: "google_oauth2") }
+  scope :github, -> { where(provider: "github") }
+
   def self.find_or_create_from_auth_hash(auth_hash, user = nil)
-    service = find_or_initialize_by(provider: auth_hash['provider'], uid: auth_hash['uid'])
-    
+    service = find_or_initialize_by(provider: auth_hash["provider"], uid: auth_hash["uid"])
+
     if service.new_record?
       # If no user is provided but we have an email, try to find a matching user
-      if user.nil? && auth_hash['info']&.key?('email')
-        user = User.find_by(email: auth_hash['info']['email'])
-        
+      if user.nil? && auth_hash["info"]&.key?("email")
+        user = User.find_by(email: auth_hash["info"]["email"])
+
         # If we still don't have a user, create one
         if user.nil?
           user = User.new(
-            email: auth_hash['info']['email'],
-            name: auth_hash['info']['name'] || auth_hash['info']['email'].split('@').first,
+            email: auth_hash["info"]["email"],
+            name: auth_hash["info"]["name"] || auth_hash["info"]["email"].split("@").first,
             password: SecureRandom.hex(24),
             password_confirmation: SecureRandom.hex(24)
           )
           user.save!
         end
       end
-      
+
       service.user = user
       service.save!
     end
-    
+
     service
   end
 end
