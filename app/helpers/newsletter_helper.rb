c4743f255853a703ec3ebcac686389dffe399102
Shivam Mishra <scm.mymail@gmail.com>
2025-05-09 13:08:28 +0530

    fix: timezone

---
+++ app/helpers/newsletter_helper.rb
@@ -0,0 +1,11 @@
+module NewsletterHelper
+  def newsletter_datetime(datetime, newsletter)
+    return unless datetime
+
+    in_zone = datetime.in_time_zone(newsletter.timezone)
+    {
+      date: in_zone.strftime("%B %d, %Y"),
+      time: in_zone.strftime("%I:%M %p")
+    }
+  end
+end
