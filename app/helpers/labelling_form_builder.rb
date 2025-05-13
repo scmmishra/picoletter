90d6e183e819397077f3af541bc6fbee76d23d0e
Shivam Mishra <scm.mymail@gmail.com>
2025-05-13 15:40:07 +0530

    test: labelled form

--- app/helpers/labelling_form_builder.rb
+++ app/helpers/labelling_form_builder.rb
@@ -1,63 +1,62 @@
 # frozen_string_literal: true
 
 class LabellingFormBuilder < ActionView::Helpers::FormBuilder
-  def text_field(attribute, options = {})
-    labelled_field(attribute, options) do
-      super(attribute, default_field_options(options))
+  def text_field(method, options = {})
+    labelled_field(method, options) do
+      super(method, default_options(options))
     end
   end
 
-  def url_field(attribute, options = {})
-    labelled_field(attribute, options) do
-      super(attribute, default_field_options(options))
+  def url_field(method, options = {})
+    labelled_field(method, options) do
+      super(method, default_options(options))
     end
   end
 
-  def text_area(attribute, options = {})
-    labelled_field(attribute, options) do
-      super(attribute, default_field_options(options))
+  def email_field(method, options = {})
+    labelled_field(method, options) do
+      super(method, default_options(options))
     end
   end
 
-  def email_field(attribute, options = {})
-    labelled_field(attribute, options) do
-      super(attribute, default_field_options(options))
+  def password_field(method, options = {})
+    labelled_field(method, options) do
+      super(method, default_options(options))
     end
   end
 
-  def password_field(attribute, options = {})
-    labelled_field(attribute, options) do
-      super(attribute, default_field_options(options))
+  def text_area(method, options = {})
+    labelled_field(method, options) do
+      super(method, default_options(options))
     end
   end
 
-  def select(attribute, choices, options = {}, html_options = {})
-    labelled_field(attribute, options) do
-      super(attribute, choices, options, default_field_options(html_options))
+  def select(method, choices, options = {}, html_options = {})
+    labelled_field(method, options) do
+      super(method, choices, options, default_options(html_options))
     end
   end
 
   private
 
-  def default_field_options(options)
-    field_options = { class: "input w-full" }
-    field_options[:class] = "#{field_options[:class]} block" if options[:block]
-    (options || {}).except(:hint).merge(field_options)
-  end
-
-  def labelled_field(attribute, options, &block)
+  def labelled_field(method, options, &block)
+    label_text = options.delete(:label) || method.to_s.humanize
     @template.content_tag(:div, class: "space-y-1") do
-      label_tag = label(attribute, class: "ml-px text-sm text-stone-500 font-sans")
-      input_tag = block_given? ? block.call : ""
-      hint_tag = build_hint(options[:hint])
-
-      label_tag + input_tag + hint_tag
+      label(method, label_text, class: "ml-px text-sm text-stone-500 font-sans") +
+        block.call +
+        hint_tag(options[:hint])
     end
   end
 
-  def build_hint(hint)
-    return "" unless hint.present?
+  def hint_tag(hint)
+    return "" unless hint
 
     @template.content_tag(:div, hint.html_safe, class: "font-sans text-xs text-stone-400")
+  end
+
+  def default_options(options)
+    field_options = { class: "input w-full" }
+    field_options[:class] += " block" if options[:block]
+    options.except(:hint, :label).merge(field_options)
   end
 end
