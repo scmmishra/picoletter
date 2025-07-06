c22b6b2b28e0b4224ae69ad93e7a7c9d746afb20
Shivam Mishra <scm.mymail@gmail.com>
2025-07-06 17:06:34 +0530

    fix: remove emoji

--- app/helpers/newsletters/posts_helper.rb
+++ app/helpers/newsletters/posts_helper.rb
@@ -1,9 +1,8 @@
 module Newsletters::PostsHelper
   def cohort_options_for_select(cohorts, selected_cohort_id = nil)
     options = cohorts.map do |cohort|
-      emoji = cohort.emoji ? "#{cohort.emoji} " : ""
       count = cohort.subscriber_count
-      text = "#{emoji}#{cohort.name} (#{count} subscribers)"
+      text = "#{cohort.name} (#{count} subscribers)"
       [ text, cohort.id ]
     end
 
