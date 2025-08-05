c3606a9ac748b5a931e9ce5e0e6af6700f3f7135
Shivam Mishra <scm.mymail@gmail.com>
2025-08-05 22:32:27 +0530

    feat: add schema information

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,8 +1,31 @@
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id            :bigint           not null, primary key
+#  role          :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  newsletter_id :bigint           not null
+#  user_id       :bigint           not null
+#
+# Indexes
+#
+#  index_memberships_on_newsletter_id              (newsletter_id)
+#  index_memberships_on_role                       (role)
+#  index_memberships_on_user_id                    (user_id)
+#  index_memberships_on_user_id_and_newsletter_id  (user_id,newsletter_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (newsletter_id => newsletters.id)
+#  fk_rails_...  (user_id => users.id)
+#
 class Membership < ApplicationRecord
   belongs_to :user
   belongs_to :newsletter
 
-  enum role: {
+  enum :role, {
     administrator: "administrator",
     editor: "editor"
   }
--- spec/models/membership_spec.rb
+++ spec/models/membership_spec.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: memberships
+#
+#  id            :bigint           not null, primary key
+#  role          :string           not null
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  newsletter_id :bigint           not null
+#  user_id       :bigint           not null
+#
+# Indexes
+#
+#  index_memberships_on_newsletter_id              (newsletter_id)
+#  index_memberships_on_role                       (role)
+#  index_memberships_on_user_id                    (user_id)
+#  index_memberships_on_user_id_and_newsletter_id  (user_id,newsletter_id) UNIQUE
+#
+# Foreign Keys
+#
+#  fk_rails_...  (newsletter_id => newsletters.id)
+#  fk_rails_...  (user_id => users.id)
+#
 require 'rails_helper'
 
 RSpec.describe Membership, type: :model do
