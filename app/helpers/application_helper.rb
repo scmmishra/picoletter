bb87cd5044c41a92bada0cd32d7d6279bc8b79f7
Shivam Mishra <scm.mymail@gmail.com>
2025-08-05 23:18:33 +0530

    feat: add permission checks to labelled form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,12 @@ module ApplicationHelper
   include Pagy::Frontend
 
   def labeled_form_with(**options, &block)
+    # Extract permission and check if form should be readonly
+    permission = options.delete(:permission)
+    if permission && @newsletter
+      options[:readonly] = !@newsletter.can_write?(permission)
+    end
+
     options[:builder] = LabellingFormBuilder
     form_with(**options, &block)
   end
