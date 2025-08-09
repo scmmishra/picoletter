f4cb8fe6c989ab9db08f87328df12b39c99bc375
Shivam Mishra <scm.mymail@gmail.com>
2025-08-09 11:10:21 +0530

    feat: invitatation controller and mailer

--- spec/mailers/previews/invitation_mailer_preview.rb
+++ spec/mailers/previews/invitation_mailer_preview.rb
@@ -1,3 +1,15 @@
-# Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer_mailer
+# Preview all emails at http://localhost:3000/rails/mailers/invitation_mailer
 class InvitationMailerPreview < ActionMailer::Preview
+  def team_invitation
+    invitation = Invitation.pending.first || Invitation.new(
+      email: "newmember@example.com",
+      role: "editor",
+      token: SecureRandom.urlsafe_base64(16),
+      newsletter: Newsletter.first,
+      invited_by: User.first,
+      expires_at: 14.days.from_now
+    )
+
+    InvitationMailer.with(invitation: invitation).team_invitation
+  end
 end
