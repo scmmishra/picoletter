147bc10c3874174f807a52131aa02267df7fe064
Shivam Mishra <scm.mymail@gmail.com>
2025-09-24 22:43:28 +0530

    feat: handle invitation flow

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -1,28 +1,17 @@
 class InvitationsController < ApplicationController
   before_action :set_invitation
   before_action :ensure_authenticated
+  before_action :ensure_invitation_present
+  before_action :ensure_invitation_belongs_to_current_user
+  before_action :ensure_invitation_active, only: [:show, :accept, :ignore]
+  before_action :redirect_if_already_member, only: [:show, :accept, :ignore]
 
   def show
-    if @invitation.accepted?
-      redirect_to_newsletter_home(notice: "You've already accepted this invitation.")
-      return
-    end
-
-    if @invitation.nil? || @invitation.expired?
-      redirect_to_newsletter_home(notice: "This invitation is no longer valid.")
-      return
-    end
-
     @newsletter = @invitation.newsletter
     @invited_by = @invitation.invited_by
   end
 
   def accept
-    if @invitation.nil? || @invitation.accepted? || @invitation.expired?
-      redirect_to_newsletter_home(notice: "This invitation is no longer valid.")
-      return
-    end
-
     if @invitation.accept!(Current.user)
       redirect_to posts_path(slug: @invitation.newsletter.slug),
                   notice: "You've successfully joined #{@invitation.newsletter.title}!"
@@ -33,11 +22,6 @@ class InvitationsController < ApplicationController
   end
 
   def ignore
-    if @invitation.nil? || @invitation.accepted? || @invitation.expired?
-      redirect_to_newsletter_home(notice: "This invitation is no longer valid.")
-      return
-    end
-
     ignored_tokens = Array(session[:ignored_invitation_tokens])
     ignored_tokens << @invitation.token unless ignored_tokens.include?(@invitation.token)
     session[:ignored_invitation_tokens] = ignored_tokens
@@ -49,5 +33,39 @@ class InvitationsController < ApplicationController
 
   def set_invitation
     @invitation = Invitation.find_by(token: params[:token])
+  end
+
+  def ensure_invitation_present
+    return if @invitation.present?
+
+    redirect_to_newsletter_home(notice: "This invitation is no longer valid.")
+    return
+  end
+
+  def ensure_invitation_belongs_to_current_user
+    return if @invitation.email.casecmp?(Current.user.email)
+
+    redirect_to_newsletter_home(alert: "This invitation was issued to a different email.")
+    return
+  end
+
+  def ensure_invitation_active
+    if @invitation.accepted?
+      redirect_to posts_path(slug: @invitation.newsletter.slug),
+                  notice: "You've already accepted this invitation."
+      return
+    end
+
+    if @invitation.expired?
+      redirect_to_newsletter_home(notice: "This invitation is no longer valid.")
+      return
+    end
+  end
+
+  def redirect_if_already_member
+    return unless @invitation.newsletter.memberships.exists?(user: Current.user)
+
+    redirect_to posts_path(slug: @invitation.newsletter.slug),
+                notice: "You're already part of this newsletter."
   end
 end
