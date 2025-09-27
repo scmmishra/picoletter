acba271926014294cd11c19682dc0abd29f84ed7
Shivam Mishra <scm.mymail@gmail.com>
2025-09-27 18:58:18 +0530

    feat: include all tokens

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -14,7 +14,7 @@ class NewslettersController < ApplicationController
   def new
     @newsletter = Current.user.owned_newsletters.new
     @new_signup = Current.user.newsletters.count.zero?
-    @pending_invitation = pending_invitation_for_current_user if @new_signup
+    @pending_invitation = pending_invitation if @new_signup
 
     render :new, layout: "application"
   end
@@ -32,5 +32,11 @@ class NewslettersController < ApplicationController
 
   def newsletter_params
     params.require(:newsletter).permit(:title, :description, :slug, :timezone)
+  end
+
+  def pending_invitation
+    scope = Invitation.pending
+                        .for_email(Current.user.email)
+                        .order(created_at: :desc).first
   end
 end
