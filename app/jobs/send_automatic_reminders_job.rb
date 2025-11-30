a2ea7fd26c0420328db8c37fec330384b2ad410f
Shivam Mishra <scm.mymail@gmail.com>
2025-11-30 09:20:25 +0530

    feat: add job for automatic reminders

---
+++ app/jobs/send_automatic_reminders_job.rb
@@ -0,0 +1,29 @@
+# This job is triggered every 30 minutes to send automatic reminders to unverified subscribers
+# who signed up approximately 24 hours ago and haven't received any reminder yet.
+class SendAutomaticRemindersJob < ApplicationJob
+  queue_as :default
+
+  def perform
+    Rails.logger.info "[SendAutomaticReminders] Starting automatic reminder processing"
+
+    Newsletter.where(auto_reminder_enabled: true).find_each do |newsletter|
+      process_newsletter(newsletter)
+    end
+
+    Rails.logger.info "[SendAutomaticReminders] Completed automatic reminder processing"
+  end
+
+  private
+
+  def process_newsletter(newsletter)
+    newsletter.subscribers.eligible_for_auto_reminder.find_each do |subscriber|
+      next if subscriber.has_delivery_issues?
+
+      Rails.logger.info "[SendAutomaticReminders] Sending reminder to subscriber #{subscriber.id} for newsletter #{newsletter.id}"
+      subscriber.send_reminder(kind: :automatic)
+    rescue StandardError => e
+      RorVsWild.record_error(e, context: { subscriber_id: subscriber.id, newsletter_id: newsletter.id })
+      Rails.logger.error "[SendAutomaticReminders] Error for subscriber #{subscriber.id}: #{e.message}"
+    end
+  end
+end
---
+++ spec/jobs/send_automatic_reminders_job_spec.rb
@@ -0,0 +1,84 @@
+require 'rails_helper'
+
+RSpec.describe SendAutomaticRemindersJob, type: :job do
+  let(:user) { create(:user) }
+  let(:newsletter) { create(:newsletter, user: user, auto_reminder_enabled: true) }
+
+  describe '#perform' do
+    context 'when auto_reminder_enabled is true' do
+      it 'sends reminders to eligible subscribers' do
+        eligible = create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+
+        expect {
+          described_class.perform_now
+        }.to have_enqueued_job(SendSubscriberReminderJob).with(eligible.id, kind: :automatic)
+      end
+
+      it 'skips subscribers with existing reminders' do
+        with_reminder = create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+        create(:subscriber_reminder, subscriber: with_reminder, kind: :manual)
+
+        expect {
+          described_class.perform_now
+        }.not_to have_enqueued_job(SendSubscriberReminderJob)
+      end
+
+      it 'skips subscribers with bounced emails' do
+        bounced = create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+        create(:email, subscriber: bounced, status: :bounced)
+
+        expect {
+          described_class.perform_now
+        }.not_to have_enqueued_job(SendSubscriberReminderJob)
+      end
+
+      it 'skips subscribers with complained emails' do
+        complained = create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+        create(:email, subscriber: complained, status: :complained)
+
+        expect {
+          described_class.perform_now
+        }.not_to have_enqueued_job(SendSubscriberReminderJob)
+      end
+
+      it 'skips verified subscribers' do
+        verified = create(:subscriber, newsletter: newsletter, status: :verified, created_at: 24.hours.ago)
+
+        expect {
+          described_class.perform_now
+        }.not_to have_enqueued_job(SendSubscriberReminderJob)
+      end
+    end
+
+    context 'when auto_reminder_enabled is false' do
+      it 'does not send any reminders' do
+        newsletter.update(auto_reminder_enabled: false)
+        create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+
+        expect {
+          described_class.perform_now
+        }.not_to have_enqueued_job(SendSubscriberReminderJob)
+      end
+    end
+
+    context 'error handling' do
+      it 'continues processing other subscribers if one fails' do
+        sub1 = create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+        sub2 = create(:subscriber, newsletter: newsletter, status: :unverified, created_at: 24.hours.ago)
+
+        # Make the first subscriber raise an error
+        allow_any_instance_of(Subscriber).to receive(:send_reminder).and_wrap_original do |method, **args|
+          if method.receiver.id == sub1.id
+            raise StandardError.new("Test error")
+          else
+            method.call(**args)
+          end
+        end
+
+        expect {
+          described_class.perform_now
+        }.to have_enqueued_job(SendSubscriberReminderJob).with(sub2.id, kind: :automatic)
+      end
+    end
+  end
+end
