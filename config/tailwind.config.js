ba05d8f73d3ecd7f36941cd46947072ac73b1ba7
Shivam Mishra <scm.mymail@gmail.com>
2025-12-04 14:55:11 +0530

    fix: fonts

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,11 +20,6 @@ module.exports = {
         "without-inset":
           "0 0 0 1px #70451a1a,0 1px 2px #70451a0a,0 3px 5px #70451a33",
       },
-      colors: {
-        newsletter: {
-          primary: "var(--color-newsletter-primary)",
-        },
-      },
     },
   },
   plugins: [
