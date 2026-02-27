07e6b3adcb1ae41f2e03048d51922a9bdf9d33a8
Shivam Mishra <scm.mymail@gmail.com>
2026-02-27 13:27:26 +0530

    feat: use custom styling for editor

--- app/jobs/base_send_job.rb
+++ app/jobs/base_send_job.rb
@@ -6,23 +6,35 @@ class BaseSendJob < ApplicationJob
   end
 
   def rendered_html_content(post, newsletter)
+    return render_html_content(post, newsletter) if Rails.env.development?
+
     Rails.cache.fetch(cache_key(post.id, "html_content"), expires_in: 2.hours) do
-      ApplicationController.render(
-        template: "publish",
-        assigns: { post: post, newsletter: newsletter },
-        layout: false
-      )
+      render_html_content(post, newsletter)
     end
   end
 
   def rendered_text_content(post, newsletter)
+    return render_text_content(post, newsletter) if Rails.env.development?
+
     Rails.cache.fetch(cache_key(post.id, "text_content"), expires_in: 2.hours) do
-      ApplicationController.render(
-        template: "publish",
-        assigns: { post: post, newsletter: newsletter },
-        layout: false,
-        formats: [ :text ]
-      )
+      render_text_content(post, newsletter)
     end
+  end
+
+  def render_html_content(post, newsletter)
+    ApplicationController.render(
+      template: "publish",
+      assigns: { post: post, newsletter: newsletter },
+      layout: false
+    )
+  end
+
+  def render_text_content(post, newsletter)
+    ApplicationController.render(
+      template: "publish",
+      assigns: { post: post, newsletter: newsletter },
+      layout: false,
+      formats: [ :text ]
+    )
   end
 end
