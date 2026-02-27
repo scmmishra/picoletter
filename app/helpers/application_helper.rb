d486addb220bcaf19778323cea73b0b38f4a5c8a
Shivam Mishra <scm.mymail@gmail.com>
2026-02-27 13:50:10 +0530

    fix: a color

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,11 +12,20 @@ module ApplicationHelper
     form_with(**options, &block)
   end
 
-  def highlight_email_code_blocks(content)
+  def highlight_email_code_blocks(content, accent_color: nil)
     return content if content.blank?
-    return content unless rouge_available?
-
     fragment = Nokogiri::HTML::DocumentFragment.parse(content.to_s)
+    accent = accent_color.presence || "#111111"
+
+    fragment.css("a").each do |anchor_node|
+      anchor_node["style"] = append_inline_style(
+        anchor_node["style"],
+        "color: #{accent} !important; text-decoration: underline;"
+      )
+    end
+
+    return fragment.to_html.html_safe unless rouge_available?
+
     formatter = Rouge::Formatters::HTMLInline.new(Rouge::Themes::Github.new)
 
     fragment.css("pre[data-language]").each do |pre_node|
@@ -49,5 +58,11 @@ module ApplicationHelper
     code_fragment = Nokogiri::HTML::DocumentFragment.parse(pre_node.inner_html.to_s)
     code_fragment.css("br").each { |node| node.replace("\n") }
     code_fragment.text
+  end
+
+  def append_inline_style(existing_style, style_to_add)
+    return style_to_add if existing_style.blank?
+
+    [ existing_style.to_s.strip, style_to_add.to_s.strip ].reject(&:blank?).join(" ")
   end
 end
